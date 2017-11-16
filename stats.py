277cdd4145a719a52bd4ebe52bf001484ab8da6a
Jan-Gerd Tenberge <janten@gmail.com>
2017-11-16 23:21:29 +0100

    First commit

---
+++ stats.py
@@ -0,0 +1,33 @@
+import os
+from time import sleep
+from redis import StrictRedis
+from influxdb import InfluxDBClient
+
+redis_url = os.environ['REDIS_URL']
+influxdb_host = os.environ['INFLUXDB_HOST']
+influxdb_port = os.environ['INFLUXDB_PORT']
+queues = os.environ.get('QUEUES', 'dataset-jobs, metadata-jobs')
+
+r = StrictRedis.from_url(redis_url)
+i = InfluxDBClient(influxdb_host, influxdb_port, "", "", 'iptk-status')
+i.create_database("iptk-status")
+
+queues = ''.join(queues.split())
+queues = queues.split(',')
+
+while True:
+    measurements = []
+    for queue in queues:
+        length = r.llen(queue)
+        measurement = {
+            "measurement": "queue_length",
+            "fields": {
+                "value": length
+            },
+            "tags": {
+                "queue": queue
+            }
+        }
+        measurements.append(measurement)
+    i.write_points(measurements)
+    sleep(10)
